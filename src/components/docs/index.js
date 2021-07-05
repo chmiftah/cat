6c80bc513663162a250f284be90fbbefcf6c43b5
enochndika <enochbeloved@gmail.com>
2021-07-05 18:57:11 +0100

    add global title and update doc

--- src/components/docs/index.js
+++ src/components/docs/index.js
@@ -241,11 +241,9 @@ export default function Docs() {
             <span className="px-1 font-medium">pages</span>directory
           </li>
           <li className="mb-3">
-            Wrap your routes in<span className="px-1 font-medium">App.js</span>
+            Wrap your routes in<span className="px-1 font-medium">app.js</span>
             by the
             <span className="px-1 font-medium">DashboardLayout</span>component
-            imported from
-            <span className="px-1 font-medium">dashboard/layout.js</span>
           </li>
         </ul>
       </section>
@@ -284,9 +282,7 @@ export default function Docs() {
             </li>
             <li className="mb-3">
               Replace your layout by the
-              <span className="px-1 font-medium">DashboardLayout</span> imported
-              from
-              <span className="px-1 font-medium">dashboard/layout.js</span>
+              <span className="px-1 font-medium">DashboardLayout</span>component
             </li>
           </ul>
         </div>
